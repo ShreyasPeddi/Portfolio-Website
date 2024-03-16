40c74867cace158c5731bb94f758a3a23e06063e
Shreyas Peddi <shreyas.peddi@gmail.com>
2024-03-16 01:18:56 -0400

    added new contact page

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -7,7 +7,6 @@ import LinkedinDarkIcon from "../../../public/linkedin_dark.png";
 import EmailDarkIcon from "../../../public/email_dark.png";
 
 const EmailSection = () => {
-
   return (
     <section
       id="contact"
@@ -15,34 +14,50 @@ const EmailSection = () => {
     >
       <div className="flex flex-col items-center">
         <div>
-          <h1 className="mt-20 text-2xl text-[#ADB7BE] mb-4 font-sans">
+          <h1 className="text-md lg:text-xl xl:mt-20 xl:text-2xl text-[#ADB7BE] mb-4 font-sans">
             Get in Touch
           </h1>
-          <h2 className="text-5xl font-bold text-white mb-4 font-sans">
+          <h2 className="text-3xl lg:text-4xl xl:text-5xl font-bold text-white mb-4 font-sans">
             Contact Me
           </h2>
         </div>
-        
-      <div className="bg-gray-200 p-6 xl:p-8 mt-10 rounded-full border border-radius border-gray-300">
-        <div className="flex flex-row gap-2 justify-center md:justify-start">
+
+        <div className="bg-gray-200 p-3 xl:p-8 mt-10 xl:w-90 rounded-full border border-radius border-gray-300">
+          <div className="flex flex-col xl:flex-row gap-2 justify-center md:justify-start">
             <div className="pl-2"></div>
-            <Link href="email.com">
-              <Image src={EmailDarkIcon} width={65} alt="Email Icon" className="w-11 h-10 pt-2 pl-1"  />
-            </Link>
-            <h2 className="text-black mt-2 text-2xl pl-2">
-              srpeddi@uwaterloo.ca
-            </h2>
-            <div className="pl-5"></div>
-            <Link href="linkedin.com">
-              <Image src={LinkedinDarkIcon} width={45} alt="Linkedin Icon" />
-            </Link>
-            <h2 className="text-black mt-2 text-2xl pr-2">
-              LinkedIn
-            </h2>
+            <div className="pt-2 flex flex-row gap-2 px-10 xl:px-0">
+              <Link href="email.com">
+                <Image
+                  src={EmailDarkIcon}
+                  width={50}
+                  alt="Email Icon"
+                  className="w-10 h-13 py-2"
+                />
+              </Link>
             
+              <h2 className="text-black mt-2 lg:text-xl xl:text-2xl pl-2">
+                srpeddi@uwaterloo.ca
+              </h2>
+
+            </div>
+
+            <div className="xl:pl-5"></div>
+            <div className="pt-2 flex flex-row gap-2 justify-center">
+              <Link href="linkedin.com">
+                <Image
+                  className=""
+                  src={LinkedinDarkIcon}
+                  width={45}
+                  alt="Linkedin Icon"
+                />
+              </Link>
+              <h2 className="text-black mt-2 lg:text-xl xl:text-2xl pr-2">
+                LinkedIn
+              </h2>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
     </section>
   );
 };
